8a15818e9813353fb764c1518f9480d9b7e6215b
Kay Hannay <khannay@psi.de>
2011-01-05 16:15:46 +0100

    -Fix efa startup -Simplify keyboard setup -Fix efaLiveSetup language file

--- config/chroot_local-hooks/03-rights.sh
+++ config/chroot_local-hooks/03-rights.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chown -R 1000:1000 /opt/efa/*
-chown -R 1000:1000 /home/efa/.ivman
+chown -R 1000:1000 /home/efa/.halevt
 chown -R 1000:1000 /home/efa/.config
 chown 1000:1000 /home/efa/.xinitrc
 #chmod 644 /etc/ivman/IvmConfigActions.xml
