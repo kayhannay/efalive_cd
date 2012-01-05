567814b2a655271e4c06730f16a5bfcb8eaac1d2
Kay Hannay <klinux@hannay.de>
2012-01-05 11:06:16 +0100

    Add version file; Adapt rights for efa diretory

--- config/chroot_local-hooks/03-rights.sh
+++ config/chroot_local-hooks/03-rights.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-chown -R 1000:1000 /opt/efa/*
+chown -R 1000:1000 /opt/efa
+chown -R 1000:1000 /opt/efa2
 chown -R 1000:1000 /home/efa/.halevt
 chown -R 1000:1000 /home/efa/.config
 chown 1000:1000 /home/efa/.xinitrc
-#chmod 644 /etc/ivman/IvmConfigActions.xml
 chmod 440 /etc/sudoers
 
