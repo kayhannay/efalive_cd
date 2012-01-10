70c6e592e3f46e076a5cdd1402039842a9dcd3bc
Kay Hannay <klinux@hannay.de>
2012-01-10 14:17:09 +0100

    -Update efaLiveSetup -Add editor

--- config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/dialogs.py
+++ config/chroot_local-includes/opt/efalive/lib/efaLiveSetup/dialogs.py
@@ -1,3 +1,22 @@
+'''
+Created on 10.01.2012
+
+Copyright (C) 2012 Kay Hannay
+
+This file is part of efaLiveSetup.
+
+efaLiveSetup is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+efaLiveSetup is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with efaLiveSetup.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -26,9 +45,15 @@ def show_error_dialog(widget, message):
     dialog.destroy()
 
 def show_exception_dialog(widget, message, details):
-    dialog = gtk.MessageDialog(widget, gtk.DIALOG_MODAL, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, message)
+    show_details_dialog(widget, gtk.MESSAGE_ERROR, _("Details"), message, details)
+
+def show_output_dialog(widget, message, details):
+    show_details_dialog(widget, gtk.MESSAGE_INFO, _("Output"), message, details)
+
+def show_details_dialog(widget, type, details_label, message, details):
+    dialog = gtk.MessageDialog(widget, gtk.DIALOG_MODAL, type, gtk.BUTTONS_CLOSE, message)
     dialog.set_resizable(True)
-    expander = gtk.Expander(_("Details"))
+    expander = gtk.Expander(details_label)
     dialog.vbox.pack_start(expander)
     expander.show()
     details_area = gtk.TextView()
